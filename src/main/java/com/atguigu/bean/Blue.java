17eb4e31376971f751a9330b2ef63b7f80550a9f
liuwenxin <1512815850@qq.com>
2023-02-12 07:18:33 +0800

    master commit

--- src/main/java/com/atguigu/bean/Blue.java
+++ src/main/java/com/atguigu/bean/Blue.java
@@ -3,5 +3,7 @@ package com.atguigu.bean;
 public class Blue {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("master test");
+        System.out.println("hot-fix test");
     }
 }
