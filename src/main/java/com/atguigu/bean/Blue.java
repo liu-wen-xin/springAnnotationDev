88c1d47834d80299458b6d3ee46e694a35a1351b
liuwenxin <1512815850@qq.com>
2023-02-12 07:19:31 +0800

    hot-fix commit

--- src/main/java/com/atguigu/bean/Blue.java
+++ src/main/java/com/atguigu/bean/Blue.java
@@ -3,5 +3,6 @@ package com.atguigu.bean;
 public class Blue {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("hot-fix test");
     }
 }
